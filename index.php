c47892f26e9b01589554ed3b834389ea87b399d0
octaviofernands <octaviofernandes.inf@gmail.com>
2012-09-20 15:37:49 -0300

    Bla

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     header('content-type: text/html; charset: utf-8');
     require_once("bingTranslate.class.php");
 
-    $bing = new BingTranslate('trendstory_translate','0WTZF6wKpVTpgCw8CBdGvCaQYU4fD1FCZe4OaS+OM0s=');
+    $bing = new BingTranslate('CLIENT_ID','CLIENT_SECRET');
 
     $translation = $bing->getTranslation("the book is on the table","en","pt");
     echo $translation["Translations"][0]["TranslatedText"];
